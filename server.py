acd2193071f4e89f5706a8cb835ac2340981de08
Rebecca <rebecca.fernands@gmail.com>
2019-05-17 02:33:09 -0300

    Subscribe e notify pronto. Falta restrição da flag

--- server.py
+++ server.py
@@ -2,16 +2,12 @@ from socket  import *
 from threading import Thread
 import os
 
-connections = []
-total_connections = 0
-
-
 def mostraPublicacoes():
 	#Printa na tela o título de todas as publicações
 	
 	todasPub = ""
 
-	for root,dirs,files in os.walk(diretorio):
+	for root,dirs,files in os.walk(publish):
 		for publicacao in files:
 			publicacao = (publicacao.split(".")[0])
 			todasPub += publicacao + "\n"
@@ -24,11 +20,11 @@ def mostraPublicacoes():
 
 def novaPublicacao(titulo, usuario, texto):
 	#cria um novo arquivo na pasta publicações com o titulo da publicação sendo o nome do arquivo, e escreve o nome do autor e o conteúdo
-	
 	try:
-		arq = open(diretorio + titulo + ".txt", "w")
+		arq = open(publish + titulo + ".txt", "w")
 		arq.write(usuario + "\n" + texto)
 		arq.close()
+		atualizaFlags(usuario)
 		return True
 
 	except:
@@ -38,8 +34,8 @@ def novaPublicacao(titulo, usuario, texto):
 def lerPublicacao(titulo):
 	#Procura publicação digitada e se existir, printa ela na tela
 
-	if(os.path.isfile(diretorio + titulo + ".txt")):
-		arq = open(diretorio + titulo + ".txt", "r")
+	if(os.path.isfile(publish + titulo + ".txt")):
+		arq = open(publish + titulo + ".txt", "r")
 		texto = arq.read()
 		arq.close()
 		return texto
@@ -50,13 +46,13 @@ def lerPublicacao(titulo):
 def deletaPublicacao(titulo, usuario):
 	#se a pessoa que tenta apagar é a autora do arquivo e ele existe, ele é apagado
 
-	if(os.path.isfile(diretorio + titulo + ".txt")):
-		arq = open(diretorio + titulo + ".txt", "r")
+	if(os.path.isfile(publish + titulo + ".txt")):
+		arq = open(publish + titulo + ".txt", "r")
 		autor = arq.readline()
 
 		if(autor == usuario + "\n"):
 			arq.close()
-			os.remove(diretorio + titulo + ".txt")
+			os.remove(publish + titulo + ".txt")
 
 			return "Publicação apagada."
 		else:
@@ -68,22 +64,54 @@ def pegaNomes():
 	#Pega o nome de todos os autores que publicaram pelo menos uma vez
 	nomes = ""
 	
-	for root,dirs,files in os.walk(diretorio):
+	for root,dirs,files in os.walk(publish):
 		for publicacao in files:
-			arq = open(diretorio + publicacao, "r")
+			arq = open(publish + publicacao, "r")
 			autor = arq.readline()
 			if autor not in nomes:
-				nomes += autor + "\n"
+				nomes += autor
 		arq.close()
 	return nomes
 
 def salvaInscricao(autor, usuario):
-	#Salva num arquivo o autor e seus inscritos
-	arq = open(subscribe + autor + ".txt", "a")
-	arq.write(usuario + " 0" + "\n")
+	#Salva num arquivo o autor e seus inscritos, desde que o autor não seja o proprio usuário
+	arq = open(subscribe + autor + ".txt", "r")
+	inscritos = arq.read()
 	arq.close()
-	return "Inscrição realizada com sucesso!"
-	
+	if (usuario == autor):
+		return "Você não pode se inscrever em si mesmo."
+	elif (usuario in inscritos):
+		return "Você já está inscrito nesse autor."
+	else:		
+		arq = open(subscribe + autor + ".txt", "a")	
+		arq.write(usuario + " 0" + "\n")
+		arq.close()
+		return "Inscrição realizada com sucesso!"
+
+def atualizaFlags(usuario):
+	#Nos arquivos de inscrição, muda pra 1 as flags de todos os inscritos desse autor
+	if(os.path.isfile(subscribe + usuario + ".txt")):
+		arq = open(subscribe + usuario + ".txt", "r")
+		inscritos = arq.read()
+		arq.close()
+		inscritos = inscritos.replace("0", "1")		
+		arq = open(subscribe + usuario + ".txt", "w")
+		arq.write(inscritos)
+		arq.close()
+
+def geraNotificacao(usuario, autor):
+	#Se usuário estiver com flag 1 em algum arquivo, muda a flag pra 0 e manda notificação daquele autor
+	arq = open(subscribe + autor, "r")
+	inscritos = arq.read()
+	arq.close()
+	if (usuario in inscritos):
+		inscritos = inscritos.replace(usuario + " 1", usuario + " 0")
+		arq = open(subscribe + autor, "w")
+		arq.write(inscritos)
+		arq.close()
+		autor = (autor.split(".")[0])
+		#manda mensagem de notificação
+		return "Você tem uma nova publicação de " + autor + "\n"
 
 def convByte(texto):
 	#transforma string em bytes
@@ -100,7 +128,7 @@ def opcoesUsuarioThread(client, ip):
 	client.send(convByte("Escreva seu nome:"))
 	usuario = client.recv(4096).decode()
 
-	client.send(convByte("\nEscreva o número da opção desejada:\n\n1 - Ver todas as publicações\n2 - Escrever uma nova publicação\n3 - Ler publicação\n4 - Apagar publicação\n5 - Se inscrever\n6 - Sair\n"))
+	client.send(convByte("\nEscreva o número da opção desejada:\n\n1 - Ver todas as publicações\n2 - Escrever uma nova publicação\n3 - Ler publicação\n4 - Apagar publicação\n5 - Se inscrever\n6 - Notificações\n7 - Sair\n"))
 
 	if(True):
 		while(True):
@@ -145,18 +173,28 @@ def opcoesUsuarioThread(client, ip):
 				continue
 
 			elif(opcao == "6"):
+				#Passa por todos os arquivos de inscrição e chama a função que gera notificação
+				try:
+					for root,dirs,files in os.walk(subscribe):
+						for autor in files:
+							client.send(convByte(geraNotificacao(usuario, autor)))
+				except:
+					client.send(convByte("Você não tem nenhuma notificação."))			
+				continue
+
+			elif(opcao == "7"):
 				client.send(convByte("Desconectado."))
 				print(str(ip) + " disconnected\n")
 				return
 
-diretorio = os.getcwd() + "\\Publicações\\"
+publish = os.getcwd() + "\\Publicações\\"
 subscribe = os.getcwd() + "\\Inscrições\\"
 
 host = "127.0.0.1"
 port = 33000
 adress = (host, port)
 
-if(not os.path.exists(diretorio)):
+if(not os.path.exists(publish)):
 	os.system("mkdir Publicações")
 
 if(not os.path.exists(subscribe)):
